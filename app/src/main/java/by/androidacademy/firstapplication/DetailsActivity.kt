029de1cd881ca1c1036edb33af695a75091dbf1e
Vasya Drobushkov <vasya.drobushkov@gmail.com>
2019-08-24 12:57:28 +0300

    Initial commit + new project with DetailsActivity created + custom text for Hello World TextView is added

---
+++ app/src/main/java/by/androidacademy/firstapplication/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package by.androidacademy.firstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
